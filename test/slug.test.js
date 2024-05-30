33f6b66597cdf636a66d7d7cd4ed89f8e8354644
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-30 15:03:19 +0200

    added second test

--- test/slug.test.js
+++ test/slug.test.js
@@ -5,11 +5,17 @@ const posts = require("../db/posts.json");
 
 // Funzione da testare
 const createSlug = (title, posts) => {
-    return title;
+    return title.toLowerCase();
 }
 
 // 1. createSlug dovrebbe ritornare una stringa
 test('createSlug dovrebbe ritornare una stringa', () => {
 
     expect(typeof createSlug('Carbonara', posts)).toBe('string');
+});
+
+// 2. createSlug dovrebbe ritornare una stringa in lowercase
+test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
+
+    expect(createSlug('Carbonara', posts) === createSlug('Carbonara', posts).toLowerCase()).toBe(true);
 });
