6a6df4cd80abab6ffbefc2459e147944ee7cc935
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-30 14:51:54 +0200

    added first test

--- test/slug.test.js
+++ test/slug.test.js
@@ -4,6 +4,12 @@ const { test, expect } = require("@jest/globals");
 const posts = require("../db/posts.json");
 
 // Funzione da testare
-const createSlug = () => {
-
+const createSlug = (title, posts) => {
+    return title;
 }
+
+// 1. createSlug dovrebbe ritornare una stringa
+test('createSlug dovrebbe ritornare una stringa', () => {
+
+    expect(typeof createSlug('Carbonara', posts)).toBe('string');
+});
