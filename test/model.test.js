762b6021cb9a1d3620a06bc48dcaef099793a6cd
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-30 17:24:30 +0200

    added third bonus test

--- test/model.test.js
+++ test/model.test.js
@@ -11,6 +11,10 @@ class Model {
 
         this.file = fileNameJson;
     }
+
+    read() {
+        return this.file;
+    }
 }
 
 // 1. Model dovrebbe essere una classe (crea un'istanza della classe Model)
@@ -22,4 +26,10 @@ test('Model dovrebbe essere una classe (crea un\'istanza della classe Model)', (
 // 2. L'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell'istanza
 test('L\'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell\'istanza', () => {
     expect(() => new Model()).toThrow();
+});
+
+// 3. L'istanza di model dovrebbe avere il metodo read
+test('L\'istanza di model dovrebbe avere il metodo read', () => {
+    const model = new Model(posts);
+    expect(model.read()).toBeDefined();
 });
