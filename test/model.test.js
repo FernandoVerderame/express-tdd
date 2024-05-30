24b9b27f98c7076142877c80d466d8e281bd8570
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-30 17:15:42 +0200

    added second bonus test

--- test/model.test.js
+++ test/model.test.js
@@ -1,12 +1,25 @@
 const { test, expect } = require("@jest/globals");
 
+const posts = require("../db/posts.json");
+
 // Classe Model
 class Model {
+    constructor(fileNameJson) {
+        if (!fileNameJson) {
+            throw new Error('Passa un file JSON.')
+        }
 
+        this.file = fileNameJson;
+    }
 }
 
 // 1. Model dovrebbe essere una classe (crea un'istanza della classe Model)
 test('Model dovrebbe essere una classe (crea un\'istanza della classe Model)', () => {
-    const model = new Model();
+    const model = new Model(posts);
     expect(model).toBeInstanceOf(Model);
+});
+
+// 2. L'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell'istanza
+test('L\'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell\'istanza', () => {
+    expect(() => new Model()).toThrow();
 });
